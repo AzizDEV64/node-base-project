b1586c8a5280744beec5b96acc40079444c6b33b
AzizDEV <programmerblack2010@gmail.com>
2026-02-07 15:06:08 +0300

    feat: Establish a dynamic routing infrastructure.

--- api/app.js
+++ api/app.js
@@ -6,8 +6,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -21,8 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', require('./routes/index'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
